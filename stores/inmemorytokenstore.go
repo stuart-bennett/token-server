1aa844ac561c538f5dbf98ac27a4d38ad91c6ea2
Stuart Bennett <stuart.bennett@bizfitech.com>
2018-11-19 09:47:40 +0000

    Synchronise access to shared token map

--- stores/inmemorytokenstore.go
+++ stores/inmemorytokenstore.go
@@ -1,14 +1,22 @@
 package stores
 
+import "sync"
+
 type InMemoryTokenStore map[string]string
+
+var mu = sync.RWMutex{}
 
 func (ts InMemoryTokenStore) NewToken(user string) string {
 	token := newToken()
+	mu.Lock()
 	ts[token] = user
+	mu.Unlock()
 	return token
 }
 
 func (ts InMemoryTokenStore) VerifyToken(token string) (string, bool) {
+	mu.RLock()
 	username, ok := ts[token]
+	mu.RUnlock()
 	return username, ok
 }
